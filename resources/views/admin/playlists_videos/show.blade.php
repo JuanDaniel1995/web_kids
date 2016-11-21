be32a977797de23002daa87d889fbf261a7d8e28
heiner96@gmail.com <heiner@gmail.com>
2016-11-20 18:38:19 -0600

    show de playlist_videos correcion2

--- resources/views/admin/playlists_videos/show.blade.php
+++ resources/views/admin/playlists_videos/show.blade.php
@@ -8,7 +8,7 @@
                 <div class="panel-heading">{{$playlist_video->playlist}}</div>
                 <div class="panel-body">
                     <div class="form-group">
-                        <label class="col-md-4 control-label">{{trans('playlists_videos.video')}}</label>
+                        <label class="col-md-4 control-label">{{trans('playlistsVideos.video')}}</label>
                         <div class="col-md-6">
                             <p>{{$playlist_video->video}}</p>
                         </div>
